5f3245cb34f13f81d38151ae61a43c158c12b4e6
Yang Wang <hyw@users.noreply.github.com>
2016-06-13 21:19:08 -0700

    another possible solution, last one didnt work

--- app/assets/javascripts/meetings/meetingCtrl.js
+++ app/assets/javascripts/meetings/meetingCtrl.js
@@ -38,7 +38,7 @@ angular.module('smartMeeting')
       }else{
         return meetings.createPublic($scope.meeting).success(function(meeting){
           $scope.meeting = meeting;
-          return $location.path('/meeting/' + meeting.access_code);
+          return $location.path('/meeting/' + meeting.access_code).replace();
         });
       }
     };
